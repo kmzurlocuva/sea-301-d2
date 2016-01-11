0d12799f18da3a4bb2ca3d52678efbfcbec605d5
Kate Zurlo-Cuva <zurlocuva@gmail.com>
2016-01-11 12:40:42 -0800

    TODOs in class for ajax and JSON

--- class-06-ajax-and-json/pair-assignment/starter-code/scripts/article.js
+++ class-06-ajax-and-json/pair-assignment/starter-code/scripts/article.js
@@ -47,16 +47,23 @@ Article.fetchAll = function() {
   if (localStorage.rawData) {
     // When rawData is already in localStorage,
     // we can load it with the .loadAll function above,
-    // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(//TODO: What do we pass in here to the .loadAll function?
+    // and then render the index page (using the proper method on the articleView object).DONE
+
+    Article.loadAll(JSON.parse(localStorage.rawData));
+    articleView.initIndexPage();
+// DONE What do we pass in here to the .loadAll function?
     );
-    articleView.someFunctionToCall; //TODO: What method do we call to render the index page?
+    articleView.someFunctionToCall; //TOWhat method do we call to render the index page?
   } else {
-    // TODO: When we don't already have the rawData,
+    // DONE When we don't already have the rawData,
     // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
     // cache it in localStorage so we can skip the server call next time,
     // then load all the data into Article.all with the .loadAll function above,
     // and then render the index page.
-
+$.getJSON('/data/hackerIpsum.json', function(rawData) {
+  Article.loadAll(rawData);
+  localStorage.rawData = JSON.stringify(rawData);
+  articleView.initIndexPage();
+    });
   }
 }
