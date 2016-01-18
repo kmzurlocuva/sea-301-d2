9b2bb7acae7cc046534d0070034f76ceb5c6af3a
Kate Zurlo-Cuva <zurlocuva@gmail.com>
2016-01-18 12:37:34 -0800

    Nadia and Kate pair program

--- class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
@@ -1,8 +1,11 @@
 (function(module) {
   var aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+
+  // DID: Define a function that hides all main section elements, and then reveals just the #about section:
   aboutController.index = function() {
+    $('.tab-content').hide();
+    $('#about').fadeIn();
 
   };
 
